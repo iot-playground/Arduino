1dae16f4b8f420b451e79ab07c09c3260614859d
iot-playground.com <igor.jarc1@gmail.com>
2014-12-23 09:41:06 +0100

    Added ESP8266 relay example
    Added ESP8266 relay example

---
+++ Esp8266EasyIoT/examples/esp8266_relay/esp8266_relay.ino
@@ -0,0 +1,60 @@
+ /*
+ V1.0 - first version
+ 
+ Created by Igor Jarc <igor.jarc1@gmail.com>
+ See http://iot-playground.com for details
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+#include <Esp8266EasyIoT.h>
+#include <SoftwareSerial.h> 
+
+Esp8266EasyIoT esp; 
+
+SoftwareSerial serialEsp(10, 11);
+
+
+
+#define RELAY_1  13  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
+#define RELAY_ON 1  // GPIO value to write to turn on attached relay
+#define RELAY_OFF 0 // GPIO value to write to turn off attached relay
+
+
+void setup()
+{
+  serialEsp.begin(9600);
+  Serial.begin(115200);  
+
+  Serial.println("EasyIoTEsp init");
+
+  esp.begin(incomingMessage, 3, &serialEsp, &Serial);
+  //esp.begin(incomingMessage, 3, &serialEsp);
+
+  pinMode(13, OUTPUT);
+
+  esp.present(1, S_DIGITAL_OUTPUT);
+}
+
+void loop()
+{
+  esp.process();
+}
+
+void incomingMessage(const Esp8266EasyIoTMsg &message) {
+  // We only expect one type of message from controller. But we better check anyway.
+
+  Serial.println("New message");
+  if (message.type==S_DIGITAL_OUTPUT) {
+    // Change relay state
+    digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
+
+    Serial.print("Incoming change for sensor:");
+    Serial.print(message.sensor);
+    Serial.print(", New status: ");
+    Serial.println(message.getBool());
+  } 
+}
+
+
