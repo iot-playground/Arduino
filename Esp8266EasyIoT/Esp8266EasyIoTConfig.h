ec81ba70930f6573f23711da5e2a288c0d382b44
iot-playground.com <igor.jarc1@gmail.com>
2015-04-18 16:54:37 +0200

    fix
    fix

--- Esp8266EasyIoT/Esp8266EasyIoTConfig.h
+++ Esp8266EasyIoT/Esp8266EasyIoTConfig.h
@@ -1,8 +1,8 @@
 #ifndef EasyIoTEspConfig_h
 #define EasyIoTEspConfig_h
 
-#define AP_SSID			"JAI"             //ACCESS POINT ID
-#define AP_PASSWORD	    "danesjelepdan"   //ACCES POINT password
+#define AP_SSID			"xxx"             //ACCESS POINT ID
+#define AP_PASSWORD	    "xxxx"   //ACCES POINT password
 #define SERVER_IP		"192.168.1.8"     //EasyIoT server IP address
 //#define SERVER_IP		"192.168.1.5"     //EasyIoT server IP address
 #define SERVER_PORT		"37602"			  //EasyIoT server port
